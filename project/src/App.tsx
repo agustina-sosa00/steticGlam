df02d3b58261aa45141781c1a023a03b08d9a483
leandro mortarini <leandromortarini2@gmail.com>
2024-07-17 13:16:57 -0300

    instalacion de Tailwind

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,14 +1,13 @@
-
-
-import './App.css'
+import "./App.css";
+import { NavBar } from "./components/NavBar/NavBar";
 
 function App() {
-
   return (
     <>
+      <NavBar />
       <h1>stetic glam</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ project/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const NavBar: React.FC = () => {
+  return <div className="w-full h-20 bg-red-700"></div>;
+};
