fb3ce3b426dd343d80e0640462c10ccdc1e6e009
Agustina <agustinaayelensosa@gmail.com>
2024-07-18 14:47:35 -0300

    andando la pagina

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -2,7 +2,6 @@
 export default {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
-<<<<<<< HEAD
     extend: { colors: {
       texto: '#000',
       // secunrdario: '#f9f3ad',
@@ -15,19 +14,6 @@ export default {
       textoAbout: ['"Parisienne"', 'cursive'],
     }},
    
-=======
-    extend: {
-      colors: {
-        texto: "#000",
-        rosa: "#f47ab7",
-        primario: "#fff",
-      },
-
-      fontFamily: {
-        titulo: ['"Great Vibes"', "cursive"],
-      },
-    },
->>>>>>> 79ad91fa18acd86e7a8f935f5988c91ead82f864
   },
   plugins: [],
 };
