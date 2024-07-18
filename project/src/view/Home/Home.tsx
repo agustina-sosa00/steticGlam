7fe35131899005b2c3df1e70bcd8698b22c81ce5
Agustina <agustinaayelensosa@gmail.com>
2024-07-18 17:49:28 -0300

    home responsive

--- project/src/view/Home/Home.tsx
+++ project/src/view/Home/Home.tsx
@@ -3,19 +3,26 @@ import './Home.css'
 
 export const Home: React.FC = () => {
   return (
-    <div className='w-full min-h-screen flex justify-start items-center '>
-       <div className='w-1/2 h-[450px] ml-10 flex flex-col justify-evenly items-start'>
-          <h3 className=' text-5xl font-texto tracking-wide'>Bienvenidos a</h3>
-          <h1 className=' text-[180px] text-secunrdario font-titulo text-center '>Stetic Glam</h1>
-            <h3 className=' text-3xl  font-texto tracking-wider'>Tu estetica de confianza</h3>
+    <div className='w-full min-h-screen flex  lg:flex-row justify-start items-center '>
+      <div className=' hidden lg:flex md:w-1/2 md:h-[450px] ml-10  flex-col justify-evenly items-start'>
+          <h3 className=' md:text-3xl lg:text-5xl font-texto md:tracking-wide'>Bienvenidos a</h3>
+          <h1 className=' text-[130px]  xl:text[150px]  2xl:text-[180px] text-secunrdario font-titulo '>Stetic Glam</h1>
+          <h3 className=' md:text-3xl  font-texto tracking-wider'>Tu estetica de confianza</h3>
           <div className='w-3/4 flex justify-end'>
            <button className='w-[140px] h-[50px] text-center rounded bg-secunrdario text-primario text-xl buttonHover tracking-wide font-semibold'>Servicios</button>  
           </div>
+      </div>
+      <div className='hidden lg:flex lg:w-1/2 min-h-screen bgHome imgEffectLeft'>
+      </div>
+      {/* para movile cambiar la imagen a una mas chica */}
+      <div className=' w-[100%] min-h-screen bgHomeMovile flex flex-col justify-center items-start lg:hidden'>
+        <h3 className='text-2xl tracking-wide mb-5 ml-5'>Bienvenidos a</h3>
+        <h1 className='text-secunrdario font-titulo text-[80px] md:text-[100px] mb-5 ml-5'>Stetic Glam</h1>
+        <h3 className='text-2xl tracking-wide mb-5 ml-5'>Tu estetica de confianza</h3>
+        <div className='w-3/4  flex justify-end sm:justify-center'>
+        <button className='bg-secunrdario w-[100px] h-[40px] text-primario text-center rounded buttonHover tracking-wide text-lg'>Servicios</button>
         </div>
-        <div className='w-1/2 min-h-screen bgHome imgEffectLeft'>
-        
-        </div>
-       
+      </div>
     </div>
   )
 }
