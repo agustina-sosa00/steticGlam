0e093c97c736844981330c0829443a431c1f8ce0
leandro mortarini <leandromortarini2@gmail.com>
2024-07-19 17:06:23 -0300

    about

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -3,13 +3,16 @@ import { NavBar } from "./components/NavBar/NavBar";
 import { Service } from "./view/Service/Service";
 import Home from "./view/Home/Home";
 import { Footer } from "./components/Footer/Footer";
+import { About } from "./view/About/About";
 
 function App() {
   return (
     <>
       <NavBar />
       <Home />
+      <About />
       <Service />
+
       <Footer />
     </>
   );
