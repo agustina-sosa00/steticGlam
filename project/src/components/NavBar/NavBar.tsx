2aa5feab082e672cde7d0d459c61d01df73e82ba
leandro mortarini <leandromortarini2@gmail.com>
2024-07-19 09:25:33 -0300

    implementacion de footer

--- project/src/components/NavBar/NavBar.tsx
+++ project/src/components/NavBar/NavBar.tsx
@@ -7,6 +7,8 @@ export const NavBar = () => {
   function handleBurger(event: React.MouseEvent) {
     event.preventDefault();
     setBurger(!burger);
+
+    console.log(burger);
   }
 
   return (
@@ -19,12 +21,12 @@ export const NavBar = () => {
         </div>
         {/* botonera */}
         <div className="hidden w-3/4 h-20  md:flex justify-evenly items-center">
-          <a href="#sec1">
+          <a href="#home">
             <button className="md:text-lg lg:text-xl xl:text-2xl font-bold text-[#f47ab7] hover:scale-110 duration-1000 hover:text-gray-800 ">
               Inicio{" "}
             </button>
           </a>
-          <a href="#sec1">
+          <a href="#about">
             <button className="md:text-lg lg:text-xl xl:text-2xl font-bold text-[#f47ab7] hover:scale-110 duration-1000 hover:text-gray-800 ">
               Conocenos
             </button>
@@ -34,7 +36,7 @@ export const NavBar = () => {
               Servicios
             </button>
           </a>
-          <a href="#sec1">
+          <a href="#contacto">
             <button className="md:text-lg lg:text-xl xl:text-2xl font-bold text-[#f47ab7] hover:scale-110 duration-1000 hover:text-gray-800 ">
               Contacto
             </button>
@@ -43,38 +45,35 @@ export const NavBar = () => {
         {/* hamburguesa */}
         <div className="w-1/2 h-20 flex justify-end items-center md:hidden ">
           <button onClick={handleBurger}>
-            {" "}
             <img src={Burger} alt="" className="w-12 mr-2" />
           </button>
         </div>
       </div>{" "}
       {/* container burger */}
       {burger === true ? (
-        <div className=" w-full h-72 flex justify-end absolute md:hidden ">
-          <div className="w-1/2 h-72 bg-[#f47ab7c2] flex flex-col justify-evenly items-end ">
-            <a href="#sec1">
+        <div className=" w-full h-72 flex justify-end  md:hidden fixed z-50 mt-20">
+          <div className="w-1/2 h-72 bg-[#f47ab7f7] flex flex-col justify-evenly items-end ">
+            <a href="#home">
               <button className="text-lg font-bold text-white hover:scale-110 duration-1000 hover:text-gray-800  mr-5">
                 Inicio
               </button>
             </a>
-            <a href="#sec1">
+            <a href="#about">
               <button className="text-lg font-bold text-white  hover:scale-110 duration-1000 hover:text-gray-800  mr-5">
                 Conocenos
               </button>
             </a>
-            href{" "}
-            <a href="#sec1">
+            <a href="#service">
               <button className="text-lg font-bold text-white  hover:scale-110 duration-1000 hover:text-gray-800  mr-5">
                 Servicios
               </button>
             </a>
-            href{" "}
-            <a href="#sec1">
+
+            <a href="#contacto">
               <button className="text-lg font-bold text-white  hover:scale-110 duration-1000 hover:text-gray-800  mr-5">
                 Contacto
               </button>
             </a>
-            href{" "}
           </div>
         </div>
       ) : null}
