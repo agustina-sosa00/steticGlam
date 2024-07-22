7aaee9eb9408a687d3a861e869c20b17941172ad
leandro mortarini <leandromortarini2@gmail.com>
2024-07-22 14:15:06 -0300

    responsive de contact

--- project/src/view/Contact/Contact.tsx
+++ project/src/view/Contact/Contact.tsx
@@ -5,50 +5,59 @@ import Ubi from "../../assets/address.svg";
 
 export const Contact: React.FC = () => {
   return (
-    <div className="w-full min-h-screen flex ">
-      <div className="w-1/2 h-screen flex flex-col items-center justify-center">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3278.788096026798!2d-58.457838225204924!3d-34.73573536448058!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccdddae39272d%3A0xe295b1a80aaac2c8!2sAmberes%202649%2C%20B1821AYA%20Ingeniero%20Budge%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1721415851255!5m2!1ses-419!2sar"
-          width="600"
-          height="600"
-          allowfullscreen=""
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
-        ></iframe>
-      </div>{" "}
-      <div className="w-1/2 h-screen flex flex-col  justify-evenly">
-        <div>
-          <h1 className=" text-[120px]  xl:text[150px]  xl:text-[100px] text-secunrdario font-titulo ">
-            Contactos
-          </h1>
-          <div className="w-3/4  m-2 flex items-center">
-            <p className="text-2xl font-medium ">
+    <div className="w-full min-h-screen flex flex-col items-center">
+      <div className="w-full  flex justify-center items-center m-5 lg:m-14 xl:m-20">
+        <h1 className="text-4xl md:text-[70px] lg:text-[80px] xl:text-[100px] text-secunrdario font-titulo ">
+          Contactos
+        </h1>
+      </div>
+      <div className="w-3/4 flex flex-col justify-center items-center  rounded-xl ">
+        <div className="w-full flex flex-col justify-center items-center ">
+          <div className="w-3/4  flex items-center">
+            <p className="md:text-lg lg:text-xl xl:text-2xl font-medium text-black text-center  ">
               Para agendar tu turno con Yohana Campos, puedes contactarnos a
               través de los siguientes medios
             </p>
           </div>
         </div>
 
-        <div className="w-1/2 h-52  flex flex-col  justify-evenly   ">
-          <div className="w-full flex items-center">
-            <img src={WP} alt="" className="w-14  " />
-            <a className="text-2xl font-bold text-green-600 ml-2 " href="###">
+        <div className="w-full  m-5  flex  flex-col md:flex-row  justify-evenly   ">
+          <div className="w-full md:w-1/3 flex flex-col items-center mb-3">
+            <img src={WP} alt="" className="w-8 md:w-10 lg:w-12 xl:w-14  " />
+            <a
+              className="md:text-md   xl:text-2xl font-bold text-green-600 ml-2 "
+              href="###"
+            >
               +54 01130768825
             </a>
           </div>
-          <div className="w-full flex items-center">
-            <img src={IG} alt="" className="w-14  " />
-            <a className="text-2xl font-bold text-pink-600 ml-2  " href="###">
+          <div className="w-full md:w-1/3 flex flex-col items-center mb-3">
+            <img src={IG} alt="" className="w-8 md:w-10 lg:w-12 xl:w-14  " />
+            <a
+              className="md:text-md lg:text-xl xl:text-2xl font-bold text-pink-600 ml-2  "
+              href="###"
+            >
               @steticglam__
             </a>
           </div>
-          <div className="w-full flex items-center">
-            <img src={Ubi} alt="" className="w-14  " />
-            <a className="text-2xl font-bold text-red-600 ml-2  " href="###">
+          <div className="w-full md:w-1/3 flex flex-col items-center mb-3">
+            <img src={Ubi} alt="" className="w-8 md:w-10 lg:w-12 xl:w-14  " />
+            <a
+              className="md:text-md lg:text-xl xl:text-2xl font-bold text-red-600 ml-2  "
+              href="###"
+            >
               Amberes 2649
             </a>
           </div>
         </div>
+      </div>
+      <div className="w-full md:w-1/2  flex flex-col items-center justify-center">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3278.788096026798!2d-58.457838225204924!3d-34.73573536448058!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccdddae39272d%3A0xe295b1a80aaac2c8!2sAmberes%202649%2C%20B1821AYA%20Ingeniero%20Budge%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1721415851255!5m2!1ses-419!2sar"
+          className="w-full h-[200px] md:w-[760px] md:h-[250px] lg:w-[1020px] lg:h-[350px] xl:w-[1200px] xl:h-[450px] mb-10"
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
       </div>
     </div>
   );
